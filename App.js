e18590fb0189cf5d7ecbe38223db7bce26aa60fc
SURYA-J <SURYA-J@github.com>
2022-05-28 20:59:11 +0530

    Enter

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
-export default function App() {
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={ProfileScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Stack = createNativeStackNavigator();
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
   },
+};
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
+        }}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default ()=>{
+  return(
+    <MyStack/>
+  )
+}
+
+/*
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import AccountScreen from './src/screens/AccountScreen.js';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+
+const switchNavigator = createSwitchNavigator({
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
 });
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+          <App />
+        );
+};
+
+*/
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+const ProfileScreen = ({ navigation, route }) => {
+    return <Text>This is  profile</Text>;
+  };
+export default ProfileScreen;
